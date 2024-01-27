9fd51e9faa7b1c6097fcecd7700f7120e225acc2
maciej88 <mkrywult88@gmail.com>
2024-01-27 08:05:47 +0100

    models.py - add tables to db

--- calib_management/models.py
+++ calib_management/models.py
@@ -1,3 +1,65 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class InstructionTypes(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Instructions(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    instruction_type = models.ForeignKey(InstructionTypes, on_delete=models.CASCADE)
+    # file = models.FileField(upload_to='...', null=True, blank=True) # todo !!!
+
+    def __str__(self):
+        return self.name
+
+
+class Places(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Services(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=128)
+    datetime = models.DateTimeField(auto_now_add=True, editable=True)
+    # file = models.FileField(upload_to='#todo') # todo!!!!
+
+    def __str__(self):
+        return self.name
+
+
+class Probe(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=128, unique=False)
+    technology_id = models.CharField(max_length=128, unique=False)
+    serial_number = models.CharField(max_length=128, unique=False)
+    factory = models.CharField(max_length=128, unique=False)
+    model = models.CharField(max_length=128, unique=False)
+    setup_date = models.DateTimeField(auto_now_add=True, editable=True) #jako data instalacji
+    place = models.ManyToManyField('Places')
+    description = models.TextField()
+    instruction = models.ManyToManyField('Instructions')
+    service = models.ManyToManyField('Services')
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Overviews(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
+    overview_date = models.DateTimeField(auto_now_add=True, editable=True)
+    probe = models.ManyToManyField('Probe')
