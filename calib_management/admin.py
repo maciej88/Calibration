9623f57b397f682ae44bb5c7de8d097b52239168
maciej88 <mkrywult88@gmail.com>
2024-01-30 18:32:49 +0100

    admin.py - add database edition via admin panel

--- calib_management/admin.py
+++ calib_management/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from calib_management.models import (
+    InstructionTypes, Instructions, Places, Services, Probe, Overviews)
+
+admin.site.register(InstructionTypes)
+admin.site.register(Instructions)
+admin.site.register(Places)
+admin.site.register(Services)
+admin.site.register(Probe)
+admin.site.register(Overviews)
