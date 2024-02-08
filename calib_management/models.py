ce94cc3be6b7ec7b3e7288e80dafd60242d413e6
maciej88 <mkrywult88@gmail.com>
2024-02-08 19:15:20 +0100

    full CRUD for Instalations, Probes and Services

--- calib_management/models.py
+++ calib_management/models.py
@@ -2,10 +2,19 @@ import uuid
 
 from django.db import models
 
+SERVICE_TYPES = (
+    ('Kalibracja', 'Kalibracja'),
+    ('Konserwacja', 'Konserwacja'),
+    ('Montaż', 'Montaż'),
+    ('Czyszczenie', 'Czyszczenie'),
+    ('Justowanie', 'Justowanie'),
+    ('Wzorcowanie', 'Wzorcowanie')
+)
+
 
 class Places(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128, unique=True, verbose_name='Obiekt: ')
 
     def __str__(self):
         return self.name
@@ -13,15 +22,18 @@ class Places(models.Model):
 
 class Probes(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    name = models.CharField(max_length=128, unique=False)
-    technology_id = models.CharField(max_length=128, unique=False)
-    serial_number = models.CharField(max_length=128, unique=False)
-    factory = models.CharField(max_length=128, unique=False)
-    model = models.CharField(max_length=128, unique=False)
-    setup_date = models.DateField(auto_now_add=True, editable=True) #jako data instalacji
-    place = models.ManyToManyField('Places')
-    description = models.TextField()
-    status = models.BooleanField(default=True)
+    name = models.CharField(max_length=128, unique=False, verbose_name='Nazwa:')
+    technology_id = models.CharField(max_length=128,
+                                     unique=False, verbose_name='Numer technologiczny:')
+    serial_number = models.CharField(max_length=128,
+                                     unique=False, verbose_name='Numer seryjny:')
+    factory = models.CharField(max_length=128, unique=False, verbose_name='Producent: ')
+    probe_model = models.CharField(max_length=128, unique=False, verbose_name='Model: ')
+    setup_date = models.DateField(editable=True, verbose_name='Data montażu: ') #jako data instalacji
+    place = models.ForeignKey('Places', verbose_name='Instalacja: ',
+                              on_delete=models.CASCADE, blank=True, null=True)
+    description = models.TextField(verbose_name='Opis: ')
+    status = models.BooleanField(default=True, verbose_name='Czy sprawna:')
 
     def __str__(self):
         return self.name
@@ -29,11 +41,12 @@ class Probes(models.Model):
 
 class Services(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    name = models.CharField(max_length=128)
-    datetime = models.DateField(auto_now_add=True, editable=True)
-    probe = models.ForeignKey(Probes, related_name='services', on_delete=models.CASCADE)
+    name = models.CharField(choices=SERVICE_TYPES, verbose_name='Rodzaj obsługi:')
+    datetime = models.DateField(editable=True, verbose_name='Data wykonania:')
+    probe = models.ForeignKey(Probes, related_name='services',
+                              on_delete=models.CASCADE, verbose_name='Obsługiwane urządzenie:')
     next_service = models.DateField(verbose_name="Termin kolejnej obsługi:")
-    # file = models.FileField(upload_to='#todo') # todo!!!!
+    description = models.TextField(max_length=512, blank=True, verbose_name='Opis wykonanych czynności:')
 
     def __str__(self):
         return self.name
