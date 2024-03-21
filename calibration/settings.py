a8083f1d0159d63ce3e5d9cea600f4708be0b5d3
maciej88 <mkrywult88@gmail.com>
2024-03-21 21:46:07 +0100

    for render deploy

--- calibration/settings.py
+++ calibration/settings.py
@@ -135,8 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
