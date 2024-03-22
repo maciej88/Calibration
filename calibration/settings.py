869bea6cecdd16ec867ea92466d1d787cec5ce3a
maciej88 <mkrywult88@gmail.com>
2024-03-22 08:57:04 +0100

    for render deploy - admin try repair

--- calibration/settings.py
+++ calibration/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
